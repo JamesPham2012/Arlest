c936c86424345346af3cda70b63ba9da1e36aa87
VGUlCS2018Uniform <52058856+VGUlCS2018Uniform@users.noreply.github.com>
2022-02-16 17:06:45 +0700

    add controller for Hotel

--- src/Modules/Hotel/HotelController.ts
+++ src/Modules/Hotel/HotelController.ts
@@ -8,21 +8,21 @@ export class HotelController {
   }
 
   @Get('/rooms') //actually not, this is to return a list of all rooms in the hotel and their info
-  getAllRoom(): string{
+  async getAllRoom() {
     return 'well prolly no rooms yet, havent got database or seeds yet bro'  ;
   }
 
   @Post('/rooms') //actually not, this is to create a new room, provided a room type and room ID
-   addRoom(@Body() abody:createRoomDTO): string{
-    return `Received request to add a new room with ${JSON.stringify(abody)} `;
+  async addRoom(@Body() abody:createRoomDTO) {
+    return `Received request to add a new room with ${JSON.stringify(abody)} and with data ${abody.RoomID}`;
   }
   @Put('/rooms') //actually not, this is to modify a room info, provided a new room type and room ID
-   modify(@Body('RoomID') RoomId, @Body('RoomType') RoomType): string{
+   modify(@Body('RoomID') RoomId, @Body('RoomType') RoomType,): string{
     return `Received request to modify room ${RoomId} to type ${RoomType} `;
   }
   @Delete('/rooms') // well 
    delRoom(@Body('RoomId') RoomId):String{
-       return `received request to delete room  ${RoomId} from records`
+       return `received request to delete  room  ${RoomId} from records`
    }
 
    
