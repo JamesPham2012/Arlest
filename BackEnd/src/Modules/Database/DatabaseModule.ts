b19e783f3edd404bc970c6e03788aee719fba24b
VGUlCS2018Uniform <52058856+VGUlCS2018Uniform@users.noreply.github.com>
2022-02-17 10:49:47 +0700

    add database Module and docker-compose

---
+++ BackEnd/src/Modules/Database/DatabaseModule.ts
@@ -0,0 +1,19 @@
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 2014,
+      username: 'admin',
+      password: 'admin',
+      database: 'BookingRDBMS',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
