9d23b6d4273bb7bd7b61f5a9beaee001f3eed98d
VGUlCS2018Uniform <52058856+VGUlCS2018Uniform@users.noreply.github.com>
2022-02-21 10:03:07 +0700

    adding A1

--- BackEnd/src/Modules/Database/DatabaseModule.ts
+++ BackEnd/src/Modules/Database/DatabaseModule.ts
@@ -1,12 +1,7 @@
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dirname } from 'path/posix';
-import { Connection } from 'typeorm';
-import { BookingOrder } from './Entities/BookingOrder';
-import { Room } from './Entities/Room';
-import { RoomTypeDetails } from './Entities/RoomTypeDetails';
-import { User } from './Entities/User';
+
 
 
 @Module({
